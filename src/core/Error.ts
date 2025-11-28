9b2a1627d987c69fb5e8453b804c7c6eedae24db
Bharat1056 <bharatpanigrahi225@gmail.com>
2025-11-29 00:14:48 +0530

    feat: Add custom  class and HTTP status constants for standardized error handling.

--- src/core/Error.ts
+++ src/core/Error.ts
@@ -1,3 +1,5 @@
+import { STATUS_CODES, STATUS_TEXTS } from '../constants/status';
+
 export class HttpError extends Error {
   status: number;
   code: string;
@@ -21,28 +23,28 @@ export class HttpError extends Error {
 // predefined errors
 export const Errors = {
   BadRequest: (msg = "Bad Request", details?: any) =>
-    new HttpError(msg, 400, "BAD_REQUEST", details),
+    new HttpError(msg, STATUS_CODES.BAD_REQUEST, STATUS_TEXTS.BAD_REQUEST, details),
 
   Unauthorized: (msg = "Unauthorized", details?: any) =>
-    new HttpError(msg, 401, "UNAUTHORIZED", details),
+    new HttpError(msg, STATUS_CODES.UNAUTHORIZED, STATUS_TEXTS.UNAUTHORIZED, details),
 
   Forbidden: (msg = "Forbidden", details?: any) =>
-    new HttpError(msg, 403, "FORBIDDEN", details),
+    new HttpError(msg, STATUS_CODES.FORBIDDEN, STATUS_TEXTS.FORBIDDEN, details),
 
   NotFound: (msg = "Not Found", details?: any) =>
-    new HttpError(msg, 404, "NOT_FOUND", details),
+    new HttpError(msg, STATUS_CODES.NOT_FOUND, STATUS_TEXTS.NOT_FOUND, details),
 
   Timeout: (msg = "Request Timeout", details?: any) =>
-    new HttpError(msg, 408, "TIMEOUT", details),
+    new HttpError(msg, STATUS_CODES.TIMEOUT, STATUS_TEXTS.TIMEOUT, details),
 
   Conflict: (msg = "Conflict", details?: any) =>
-    new HttpError(msg, 409, "CONFLICT", details),
+    new HttpError(msg, STATUS_CODES.CONFLICT, STATUS_TEXTS.CONFLICT, details),
 
   Internal: (msg = "Internal Server Error", details?: any) =>
-    new HttpError(msg, 500, "INTERNAL_ERROR", details),
+    new HttpError(msg, STATUS_CODES.CONFLICT, STATUS_TEXTS.INTERNAL_SERVER_ERROR, details),
 
   ServiceUnavailable: (msg = "Service Unavailable", details?: any) =>
-    new HttpError(msg, 503, "SERVICE_UNAVAILABLE", details),
+    new HttpError(msg, STATUS_CODES.CONFLICT, STATUS_TEXTS.SERVICE_UNAVAILABLE, details),
 };
 
 // usage:
